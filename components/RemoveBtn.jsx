e6fe200ff6f95cc17e9fc15b766c34d14b72e06c
Maria79 <mariajalbisu@gmail.com>
2023-06-25 10:57:50 +0100

    now

--- components/RemoveBtn.jsx
+++ components/RemoveBtn.jsx
@@ -1,14 +1,22 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
 import React from 'react';
 import { HiOutlineTrash } from 'react-icons/hi';
 
 export default function RemoveBtn({ id }) {
+	const router = useRouter();
 	const removeTopic = async () => {
 		const confirmed = confirm('Are you sure?');
 
 		if (confirmed) {
-			await fetch(`http://localhost:3000/api/topics?id=${id}`, {
+			const res = await fetch(`http://localhost:3000/api/topics?id=${id}`, {
 				method: 'DELETE',
 			});
+
+			if (res.ok) {
+				router.refresh();
+			}
 		}
 	};
 	return (
--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -8,6 +8,7 @@ const getTopics = async () => {
 		const res = await fetch('http://localhost:3000/api/topics', {
 			cache: 'no-store',
 		});
+
 		if (!res.ok) {
 			throw new Error('Failed to fetch topics.');
 		}
